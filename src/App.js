223b4ad0cdeec3639d912522b5ac5a08600dea42
Dan Green <danngreen1@gmail.com>
2017-11-26 21:53:51 -0800

    Finished project. Yelp API is accessed, all works

--- src/App.js
+++ src/App.js
@@ -2,14 +2,28 @@ import React from 'react';
 import './App.css';
 import BusinessList from './components/BusinessList/BusinessList';
 import SearchBar from './components/SearchBar/SearchBar';
+import { Yelp } from './util/Yelp';
 
 class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = { businesses: [] };
+    this.searchYelp = this.searchYelp.bind(this);
+  }
+
+  searchYelp(term, location, sortBy){
+    Yelp.search(term, location, sortBy).then( b => {
+      this.setState({ businesses: b });
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <h1>ravenous</h1>
-        <SearchBar />
-        <BusinessList />
+        <SearchBar searchYelp={this.searchYelp}/>
+        <BusinessList businesses={this.state.businesses} />
       </div>     
     );
   }
