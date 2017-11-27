c8cb81f3fcc26f65b52ba7abd7b0479ef6add32e
Dan Green <danngreen1@gmail.com>
2017-11-26 22:28:42 -0800

    Pressing enter in either text box initiates search

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -13,6 +13,7 @@ class SearchBar extends React.Component {
 		this.handleTermChange 		= this.handleTermChange.bind(this);
 		this.handleLocationChange	= this.handleLocationChange.bind(this);
 		this.handleSearch			= this.handleSearch.bind(this);
+		this.handleKeyUp			= this.handleKeyUp.bind(this);
 	}
 
 	sortByOptions = {
@@ -44,6 +45,11 @@ class SearchBar extends React.Component {
 		e.preventDefault();
 	}
 
+	handleKeyUp(e){
+		if(e.key !== "Enter") return;
+		this.handleSearch(e);
+	}
+
 	renderSortByOptions(){
 		return Object.keys(this.sortByOptions).map( sortByOption => {
 			let sortByOptionValue = this.sortByOptions[sortByOption];
@@ -66,8 +72,8 @@ class SearchBar extends React.Component {
 			    </ul>
 			  </div>
 			  <div className="SearchBar-fields">
-			    <input placeholder="Search Businesses" onChange={this.handleTermChange}/>
-			    <input placeholder="Where?"  onChange={this.handleLocationChange}/>
+			    <input placeholder="Search Businesses" onChange={this.handleTermChange} onKeyPress={this.handleKeyUp}/>
+			    <input placeholder="Where?"  onChange={this.handleLocationChange} onKeyPress={this.handleKeyUp}/>
 			  </div>
 			  <div className="SearchBar-submit">
 			    <a onClick={this.handleSearch}>Let's Go</a>
